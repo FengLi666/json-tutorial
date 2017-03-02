d66fcabedf567ab3c14f84cd7afd465c075e1d47
FengLi666 <fengli97@hotmail.com>
2017-03-02 23:01:16 +0800

    complete ch01

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -15,18 +15,23 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
-static int lept_parse_null(lept_context* c, lept_value* v) {
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_NULL;
+static int lept_parse_equal_dest_string(lept_context* c, lept_value* v , const char* dest , size_t length , lept_type test_type) {
+    EXPECT(c, dest[0]);
+    dest++;
+    for (int i = 1; i < length -1; ++i) {
+        if(c->json[i] != dest[i])
+            return  LEPT_PARSE_INVALID_VALUE;
+    }
+    c->json += length-1;
+    v->type = test_type;
     return LEPT_PARSE_OK;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);
+        case 'n':  return lept_parse_equal_dest_string(c,v,"null",4,LEPT_NULL);
+        case 'f': return lept_parse_equal_dest_string(c,v,"false",5 ,LEPT_FALSE);
+        case 't': return lept_parse_equal_dest_string(c,v,"true",4 , LEPT_TRUE);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
@@ -38,7 +43,14 @@ int lept_parse(lept_value* v, const char* json) {
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    int result = lept_parse_value(&c, v);
+    lept_parse_whitespace(&c);
+    if (result == LEPT_PARSE_OK && *c.json != '\0')
+    {
+            return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    else
+        return result;
 }
 
 lept_type lept_get_type(const lept_value* v) {
